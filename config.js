7cbd4b8b8fda0568c55b327521164b6d953c1b6f
Senkku4098 <germainadley24@gmail.com>
2024-12-07 23:10:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "SBIQwIjZ#3oea15EvHNssg9I6KaFdErV0Ty9304S3h8JrWiHYcAk",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
-    MODE: process.env.MODE || "public",
+    MODE: process.env.MODE || "private",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
@@ -19,10 +19,10 @@ module.exports = {
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
-    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "true",
-    HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    BOT_NAME: process.env.BOT_NAME || "➺𖤓⃞💚༒𝗦𝗲𝗻𝗸𝘂 𝗜𝘀𝗵𝗶𝗴𝗮𝗺𝗶༒𖤓⃞🗿࿐",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
